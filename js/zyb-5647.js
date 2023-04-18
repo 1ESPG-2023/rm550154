87b68d3ead39cf6186eaa37ad1ca48b35862a424
KevinBeykam <rm550154@fiap.com.br>
2023-04-18 11:07:19 -0300

    agregand o map

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,51 +1,56 @@
-// const liElement = document.getElementsByTagName("li");
+// const liElement = document.getElementsByTagName("li") //utiliza de uma coleção de objetos no geral, utilizando do seu nome e não do seu id/sua identificação
 
-// for (let index = 0; index < liElement.length ; index++) {
-//     console.log(liElement[index].textContent)
+// // for (let index = 0; index < liElement.length; index++) {
+// //     console.log(liElement[index].textContent)
+// // }
+
+// //console.log(liElement.length)
+
+// //Declaração de Arrays
+// let num1 = [1,2,3,4,5];
+// let num2 = [6,7,8,9,10];
+// let num3 = [num1,num2];
+
+// console.log(`Array-1 ${num1}`);
+// console.log(`Array-2 ${num2}`);
+// console.log(`Array-3 ${num3}`);
+
+// //Listando um Array! Técnica especial
+// // num3.forEach((itemDoArray)=>{
+// //     console.log(`Array-3 sendo listado ${itemDoArray}`);
+// // })
+
+//______________________________________________________________________________18/04/2023
+// //Listando um Array com MAP
+// num3.map((numero,key)=>{
     
-// }
-// console.log(liElement.length);
-
-//Declaração de Arrays
-// let nr1 = [1,2,3,4,5];
-// let nr2 = [6,7,8,9,10];
-// let nr3 = [nr1,nr2]
-
-// console.log(`Array-1 ${nr1}`);
-// console.log(`Array-2 ${nr2}`);
-// console.log(`Array-3 ${nr3}`);
-
-//Listando um array! Técnica Especial!
-// nr3.forEach((ItemDoArray)=>{
-//     console.log(`Array-3 sendo listado ${ItemDoArray}`);
-// })
-
-// Listando o Array com MAP
-// nr3.map((numero,key)=>{
-//     console.log(`${key+1} - Itens do array - ${numero}`);
+//     console.log(`${key + 1} - Itens do Array - ${numero}`);
 //     numero.forEach((digitos)=>{
-//         console.log(digitos)
+//         console.log(digitos);
 //     })
 // })
 
-//Declaração de Arrays para uso do SPREAD
-let nr1 = [1,2,3,4,5];
-let nr2 = [6,7,8,9,10];
-// Espalhar o array com SPREAD [ ... ]
-let nr3 = [...nr1,...nr2]
+// const liElement = document.getElementsByTagName("li") //utiliza de uma coleção de objetos no geral, utilizando do seu nome e não do seu id/sua identificação
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// //Declaração de Arrays
+// let num1 = [1,2,3,4,5];
+// let num2 = [6,7,8,9,10];
+// //Espalhar o Array com o SPREAD > AGORA OS OUTROS ARRAYS ESTÃO DESTRIBUÍDOS AQUI DENTRO, NÃO ESTÃO APENAS SENDO JOGADOS AQUI
+// let num3 = [...num1,...num2];
 
-nr3.map((numero,key)=>{
-     console.log(`${key+1} - Itens do array - ${numero}`);
-})
+// console.log(`Array-1 ${num1}`);
+// console.log(`Array-2 ${num2}`);
+// console.log(`Array-3 ${num3}`);
 
-// Convertendo HTMLCollection em Array de forma indireta
-const liElementHtml = document.getElementsByTagName("li");
-const liElementArray = [...liElementHtml];
+// // num3.forEach((itemDoArray)=>{
+// //      console.log(`Array-3 sendo listado ${itemDoArray}`);
+// //  })
 
-liElementArray.map((item,key)=>{
-    console.log(`${key+1} - Itens do li - ${item.textContent}`);
-})
+// //Convertendo HTMLCollection em Array de forma indireta
+// const liElementHTML = document.getElementsByTagName("li");
+// const liElementArray = [...liElementHTML];
+
+// liElementArray.map((item,key)=>{
+//     if (item.textContent == "Item-39"){
+//         console.log(`${key + 1} - ${item.textContent = "LOCALIZADO"}`);
+//     }
