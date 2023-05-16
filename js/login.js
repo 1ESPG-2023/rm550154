12a64e46ceca1c271344190a829cb9539a4b9e48
KevinBeykam <rm550154@fiap.com.br>
2023-05-16 11:49:28 -0300

    html att novo

--- js/login.js
+++ js/login.js
@@ -1,26 +1,29 @@
-//GERANDO DOIS OBJETOS NO LOCALSTORGE
+
+//GERANDO DOIS OBJETOS NO LOCALSTORAGE
 const usuario1 = {
     nomeUsuario:"predo",
     senhaUsuario:"12345"
 }
 
 const usuario2 = {
-    nomeUsuario:"anzinha",
+    nomeUsuario:"anzina",
     senhaUsuario:"12345"
 }
 
-let listaDeUsuarios = []
-listaDeUsuarios.push(usuario1)
-listaDeUsuarios.push(usuario2)
+let listaDeUsuarios = [];
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
 
-localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
+localStorage.setItem("listaUser",  JSON.stringify(listaDeUsuarios));
 
 
 //VAMOS CRIAR UM OBJETO PARA ARMAZENAR O NOSSO USUÁRIO
-//const usuario = {
- //   nomeUsuario:"",
-  //  senhaUsuario:""
-//}
+// const usuario = {
+//     nomeUsuario:"",
+//     senhaUsuario:""
+// }
+
+
 
 addEventListener("click",(evento)=>{
 
@@ -29,26 +32,77 @@ addEventListener("click",(evento)=>{
     if(evento.target.id == "btnSubmit"){
         // let usuario = document.querySelector("#idUser").value;
         // let senha = document.querySelector("#idPass").value;
-
-        //RECUPERANDO DOS IMPUTS
+       
+        // RECUPERANDO DOS IMPUTS
         let inputUserValue = document.querySelector("#idUser").value;
-        let inputPassValue= document.querySelector("#idPass").value;
-
+        let inputPassValue = document.querySelector("#idPass").value;
+        
         const h1Titulo = document.querySelector("#titulo");
 
         let lista = JSON.parse(localStorage.getItem("listaUser"));
 
-        lista.forEach(usuario=> {
-            //VALIDAÇÃO
-        if(inputUserValue == usuario.nomeUsuario && inputPassValue == usuario.senhaUsuario){
-            console.log("VALIDADO!");   
-            h1Titulo.innerHTML = "Bem vindo : " + usuario.nomeUsuario;
-        }else{
-            console.log("NÃO VALIDOU!");
-            h1Titulo.innerHTML = "";
-         
-        }
-        });
+        let userValidado = {};
+        
+        try{
+            lista.forEach((usuario)=> {
+                //VALIDAÇÃO
+                if(inputUserValue == usuario.nomeUsuario && inputPassValue == usuario.senhaUsuario){
+                    userValidado = usuario;
+                    throw "VALIDADO";
+                }
+            });
 
- }
+                throw "NÃO VALIDADO";
+
+        }catch(msg){
+            if(msg == "VALIDADO"){
+                h1Titulo.innerHTML = "<span><strong>Login validado com sucesso!</strong></span>";
+                h1Titulo.setAttribute("style","color:#00ff00;");
+
+                //Adicionando uma propriedade ao nosso objeto userValidado
+                userValidado["token"] = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
+
+                //Setando um novo objeto no LocalStorage
+                localStorage.setItem("UserValidado",  JSON.stringify(userValidado));
+                //Direcionando o usuário para a página de sucesso!
+                window.location.href = "../sucesso.html";
+
+            }else{
+                h1Titulo.innerHTML = "<span><strong>Login ou senha inválidos!</strong></span>";
+                h1Titulo.setAttribute("style","color:#ff0000;");
+                window.location.href = "../erro.html";
+            }
+        }       
+    }
 });
+
+try{
+    const userBemVindo = document.querySelector("#userWelcome");
+    let usuario = JSON.parse(localStorage.getItem("UserValidado"));
+    
+    if(usuario){
+        
+        if(usuario.token){
+            userBemVindo.innerHTML = usuario.nomeUsuario;
+        }
+
+    }else{
+        window.location.href = "../erro.html";
+    }
+
+    const botaoLogout = document.querySelector("#btnLogout");
+    botaoLogout.addEventListener("click", ()=>{
+        localStorage.removeItem("UserValidado");
+        window.location.href = "../login.html";
+    });
+
+}catch(erro){
+
+    if(userBemVindo != null){
+        userBemVindo.innerHTML = JSON.parse(localStorage.getItem("UserValidado")).nomeUsuario;
+    }
+}
+
+
+
+
